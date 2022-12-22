b68b0ce5b4cbbafdaea8746dd7d4e1af7419f3c2
Pavel Afanasyev <afanasyevpv9@gmail.com>
2022-12-22 16:17:03 +0300

    Реализовано домашнее задание - приложение-счётчик

--- Sprint_3_HomeWork_CounterApp/ViewController.swift
+++ Sprint_3_HomeWork_CounterApp/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var counter = 0
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBAction func addButttonTapped(_ sender: Any) {
+        counter += 1
+        countLabel.text = "Значение счетчика: \(counter)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addButton.layer.cornerRadius = 10
     }
-
-
 }
-
